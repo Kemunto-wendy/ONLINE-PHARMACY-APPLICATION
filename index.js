efaf74a30b1094a3e0bffb9679b595e05ca31fed
JOY <joy.kemunto@student.moringaschool.com>
2022-07-25 17:38:12 +0300

    Changes

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+
+console.log("joy Kmeunto")
 function myFunction(){
     document.getElementById("header").innerHTML = "Hello this is my new project"
+    window.print("print the code")
 }
+
+return myFunction()
