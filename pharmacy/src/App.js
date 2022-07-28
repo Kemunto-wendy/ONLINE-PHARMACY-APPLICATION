2d54a2444909ee7b654206660b0180d1ae6e7dec
JOY <joy.kemunto@student.moringaschool.com>
2022-07-28 11:06:48 +0300

    Changes

--- pharmacy/src/App.js
+++ pharmacy/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Header from './components/Header/header';
+
 
 function App() {
+
   return (
   <div>
-  ONLINE PHARMACY
+
+<Header />
   </div>
   );
 }
---
+++ pharmacy/src/components/Header/header.js
@@ -0,0 +1,10 @@
+import "./header.css"
+const Header = props =>{
+    return(
+        <div className="header-page">
+            ONLINE PHARMACY STORE
+        </div>
+    )
+}
+
+export default Header
