0b5b50b4b7c7aa3c6c7f63006e6d099edb72a0fd
JOY <joy.kemunto@student.moringaschool.com>
2022-07-28 11:12:43 +0300

    changes

--- pharmacy/src/components/Header/header.js
+++ pharmacy/src/components/Header/header.js
@@ -1,8 +1,20 @@
 import "./header.css"
 const Header = props =>{
     return(
+        <div className="header">
         <div className="header-page">
-            ONLINE PHARMACY STORE
+        <img src="images/logo1.png" class="logo" />   
+        ONLINE PHARMACY STORE
+        </div>
+        <div className="homepage">
+        <ul>
+            <li><a href="#">HOME</a></li>
+            <li><a href="#">DIET</a></li>
+            <li><a href="#">EXCERCISE</a></li>
+            <li><a href="#">LIFESTYLE</a></li>
+            <li><a href="#">RECOMMENDATION</a></li>
+        </ul>
+        </div>
         </div>
     )
 }
